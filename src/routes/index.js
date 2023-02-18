d7946750c61df0324784f8faaf3b6c92d74d7aee
Mat <mat@Mats-MacBook-Pro.local>
2023-02-18 07:54:53 +0100

    lol

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const { conn } = require('../services/connToDB');
+//const { conn } = require('../services/connToDB');
 
 const helloWorldController = require('../controllers/hello-world');
 const LoginController = require('../controllers/loginCont');
@@ -10,22 +10,22 @@ const Authentication = require('../services/authServ');
 const RegistrationService = require('../services/regServ');
 const RegistrationController = require('../controllers/regCont');
 
-let useddb = conn;
+//let useddb = conn;
 let accTokSec = process.env.ACCESS_TOKEN_SECRET;
 let refTokSec = process.env.REFRESH_TOKEN_SECRET;
 
 
-const registrationService = new RegistrationService(useddb);
+//const registrationService = new RegistrationService(useddb);
 const auth = new Authentication(accTokSec, refTokSec);
-const registrationController = new RegistrationController(registrationService, Authentication.getRoleFromToken);
-const loginService = new LoginService(useddb, registrationService, auth.generateAccessToken, auth.generateRefreshToken);
-const loginController = new LoginController(loginService);
+//const registrationController = new RegistrationController(registrationService, Authentication.getRoleFromToken);
+//const loginService = new LoginService(useddb, registrationService, auth.generateAccessToken, auth.generateRefreshToken);
+//const loginController = new LoginController(loginService);
 
 
 router.get('/helloworld', helloWorldController.helloWorldController);
 
-router.post('/login', loginController.login);
+//router.post('/login', loginController.login);
 
-router.post('/register', auth.authenticateToken, registrationController.register);
+//router.post('/register', auth.authenticateToken, registrationController.register);
 
 module.exports = router;
